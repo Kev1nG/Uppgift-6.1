0feed5472ef20b860b777c5cd4fe20f41257d149
Pingvu <nhlpool27@gmail.com>
2023-03-09 13:49:32 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace uppgift_6._1
+{
+    class program
+    {
+        static double Addera(double x, double y, double b)
+        {
+            return(x + y + b);
+        }
+        public static void Main()
+        {
+            Console.WriteLine(Addera(1.5, 2.8, 3.9));
+        }
+        
+
+
+    }
+}
